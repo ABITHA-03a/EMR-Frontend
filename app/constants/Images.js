dc3df697f23a1d3ce4c3a7c97e870187dd3d5c12
ABITHA <23pca149@anjaconline.org>
2025-05-22 15:06:41 +0530

    Revert "Initial commit of login and register screens"
    This reverts commit 146bb72c36dee165e59b3e17fd26387effc3ee19.

--- app/constants/Images.js
+++
@@ -1,7 +0,0 @@
-// D:\my-login-app\app\constants\Images.js
-
-export const carouselImages = [
-  // --- CHANGE THESE PATHS ---
-  require('../../assets/images/pot.jpg'), // Corrected path!
-   
-];
--- app/constants/backgrounds.js
+++
@@ -1,9 +0,0 @@
-// D:\my-login-app\app\constants\backgrounds.js
-// Update these paths to match your renamed files
-import bg1 from '../../assets/images/bg_image_01.jpg'; // Changed filename
-import bg2 from '../../assets/images/bg_image_02.jpg'; // Changed filename
-
-export const backgroundImages = [
-  bg1,
-  bg2,
-];
